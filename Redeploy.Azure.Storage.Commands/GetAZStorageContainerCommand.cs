cd40ea5dfb719956e8b75f4f889589a4c14566f6
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-23 23:53:02 +0100

    Initial command file

--- Redeploy.Azure.Storage.Commands/GetAZStorageContainerCommand.cs
+++ Redeploy.Azure.Storage.Commands/GetAZStorageContainerCommand.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Management.Automation;
 using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace Redeploy.Azure.Storage.Commands
 {
