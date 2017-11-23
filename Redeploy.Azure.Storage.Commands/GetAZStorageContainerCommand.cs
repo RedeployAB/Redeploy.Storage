ab7cee6c121a2d634a67f1da269bcdf676439762
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-23 23:32:11 +0100

    First commit

---
+++ Redeploy.Azure.Storage.Commands/GetAZStorageContainerCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace Redeploy.Azure.Storage.Commands
+{
+    [OutputType(typeof(CloudBlobContainer))]
+    [Cmdlet("Get", "AZStorageContainer")]
+    public class GetAZStorageContainerCommand : Cmdlet
+    {
+    }
+}
---
+++ Redeploy.Azure.Storage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Redeploy.Azure.Storage
+{
+    public class Class1
+    {
+    }
+}
