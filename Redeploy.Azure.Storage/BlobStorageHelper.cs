d793c983e6a4724143b75515d6185afd2c972b05
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-23 23:52:45 +0100

    New class to handle blobs

---
+++ Redeploy.Azure.Storage/BlobStorageHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace Redeploy.Azure.Storage
+{
+    public class BlobStorageHelper
+    {
+        public CloudStorageAccount StorageAccount;
+
+        public BlobStorageHelper(string storageAccountName, string storageAccountKey)
+        {
+            StorageAccount = new CloudStorageAccount(
+                new StorageCredentials(storageAccountName, storageAccountKey), true);
+        }
+
+        public BlobStorageHelper(StorageCredentials storageCredentials)
+        {
+            StorageAccount = new CloudStorageAccount(storageCredentials, true);
+        }
+    }
+}
