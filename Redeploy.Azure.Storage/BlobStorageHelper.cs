49c2859464eb38e443dc3ff579933712aae3a98f
Karl Wallenius <karl.wallenius@gmail.com>
2017-11-24 12:27:19 +0100

    Added some class methods

--- Redeploy.Azure.Storage/BlobStorageHelper.cs
+++ Redeploy.Azure.Storage/BlobStorageHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.WindowsAzure.Storage;
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Auth;
 
@@ -17,6 +18,47 @@ namespace Redeploy.Azure.Storage
         public BlobStorageHelper(StorageCredentials storageCredentials)
         {
             StorageAccount = new CloudStorageAccount(storageCredentials, true);
+        }
+
+        public CloudBlobContainer GetContainer(string containerName)
+        {
+            CloudBlobClient blobClient = StorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            var exists = _containerExists(containerName).Result;
+
+            if (!exists)
+                container = null;
+
+            return container;
+        }
+
+        public async Task<CloudBlob> UploadBlob(string containerName, string filePath, string blob)
+        {
+
+            CloudBlobClient blobClient = StorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blob);
+
+            using (var fileStream = System.IO.File.OpenRead(filePath))
+            {
+                await blockBlob.UploadFromStreamAsync(fileStream);
+            }
+
+            CloudBlockBlob resultBlob = container.GetBlockBlobReference(blob);
+
+            return resultBlob;
+        }
+
+        private async Task<bool> _containerExists(string containerName)
+        {
+            CloudBlobClient blobClient = StorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            var result = await container.ExistsAsync();
+
+            return result;
         }
     }
 }
