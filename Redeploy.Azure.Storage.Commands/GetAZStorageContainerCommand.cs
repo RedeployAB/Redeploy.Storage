38182cf1cfa9be812dea9cbfd5a2094b027cf0c5
Karl Wallenius <karl.wallenius@gmail.com>
2017-12-12 10:26:33 +0100

    Added meat to the code

--- Redeploy.Azure.Storage.Commands/GetAZStorageContainerCommand.cs
+++ Redeploy.Azure.Storage.Commands/GetAZStorageContainerCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Management.Automation;
-using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Blob; 
+using Microsoft.WindowsAzure.Storage.Auth;
+using Redeploy.Azure.Storage;
 
 namespace Redeploy.Azure.Storage.Commands
 {
@@ -8,5 +10,50 @@ namespace Redeploy.Azure.Storage.Commands
     [Cmdlet("Get", "AZStorageContainer")]
     public class GetAZStorageContainerCommand : Cmdlet
     {
+        [ValidateNotNull]
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            HelpMessage = "A StorageCredential object. Create one with 'New-AZStorageCredential'."
+        )]
+        public StorageCredentials StorageCredential
+        {
+            get { return _storageCredential; }
+            set { _storageCredential = value; }
+        }
+
+        private StorageCredentials _storageCredential;
+
+        [ValidateNotNullOrEmpty]
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            HelpMessage = "Name of Container in Storage Account"
+        )]
+        public string ContainerName
+        {
+            get { return _containerName; }
+            set { _containerName = value; }
+        }
+
+        private string _containerName;
+
+        protected override void ProcessRecord()
+        {
+            StorageCredentials credential = _storageCredential;
+            BlobStorageHelper storageHelper = new BlobStorageHelper(credential);
+            CloudBlobContainer container;
+
+            try
+            {
+                container = storageHelper.GetContainer(_containerName);
+            }
+            catch (System.AggregateException exception)
+            {
+                throw new System.AggregateException(exception);
+            }
+
+            WriteObject(container);
+        }
     }
 }
