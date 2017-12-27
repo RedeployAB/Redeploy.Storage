b4cc688ef89a1d6921501f29071114d4be9b066b
Karl Wallenius <karl.wallenius@gmail.com>
2017-12-27 17:42:46 +0100

    Added model/class for StorageContext

---
+++ Redeploy.Azure.Storage/Models/StorageContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
+
+namespace Redeploy.Azure.Storage.Models
+{
+    public class StorageContext
+    {
+        public StorageContext(string storageAccountName, string storageAccountKey)
+        {
+            StorageAccount = new CloudStorageAccount(
+                new StorageCredentials(storageAccountName, storageAccountKey), true);
+
+            StorageAccountName = storageAccountName;
+            BlobEndpoint = StorageAccount.BlobEndpoint;
+            TableEndPoint = StorageAccount.TableEndpoint;
+            QueueEndpoint = StorageAccount.QueueEndpoint;
+            FileEndPoint = StorageAccount.FileEndpoint;
+        }
+
+        public StorageContext(StorageCredentials credentials)
+        {
+            StorageAccount = new CloudStorageAccount(credentials, true);
+        }
+
+        public string StorageAccountName { get; }
+        public Uri BlobEndpoint { get; }        
+        public Uri TableEndPoint { get; }
+        public Uri QueueEndpoint { get; }
+        public Uri FileEndPoint { get; }
+         public CloudStorageAccount StorageAccount { get; set; }
+
+        public string EndPointSuffix { get; } = "core.windows.net";
+    }
+}
